5148d94c0f5125cc745bc83415914ee2ee1fa0c5
Thesergioandres <127797151+Thesergioandres@users.noreply.github.com>
2023-10-05 00:23:50 -0500

    Tercer Avance

--- proyecto/Proyecto inta/Main/Main-instagram/proyecto nicolas/js/mas.js
+++
@@ -1,6 +0,0 @@
-const masContainer = document.querySelector(".menu_mas");
-const mas = document.querySelector(".mas");
-
-mas.addEventListener("click", () => {
-masContainer.classList.toggle("actives");
-});
